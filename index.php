84487147f01432a80a64168b4ae0b42da3a55c0e
RevatiPatil-123 <rrp29101999@gmail.com>
2021-08-16 15:57:43 +0530

    index page

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>UPSA LMS</title>
+		<link href="../css/public.css" media="all" rel="stylesheet" type="text/css" />
+	</head>
+	<body bgcolor="#FFFF66">
+	<div id="header">
+	  <h1>UPSA VIRAL</h1>
+	</div>
+	<div id="main">
+	   <link href="css/public.css" media="all" rel="stylesheet" type="text/css" />
+	   <table id="structure">
+	   <tr>
+	        <td id="navigation">
+    		<ul>
+		    	<li class="last"><a href="student.php">Student detail</a></li><br/>
+			    <li class="last"><a href="login.php">Login Issues</a></li><br/>
+			    <li class="last"><a href="course.php">Course Issues</a></li><br/>
+			    <li class="last"><a href="other.php">Other Issues</a></li><br/>
+			</ul>			
+		</td>
+		<td id="page">
+			<h2> UPSA VIRAL LMS </h2>
+		</td>
+	   </tr>
+	  </table>
+	</div>
+	<div id="footer" title = "Designed & Developed by REV">UPSA LMS </div>
+	</body>
+</html>
